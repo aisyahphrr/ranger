642804060b34c782bb12980424bdcedba9f875aa
aisyahphrr <aisyahputrihhh16@gmail.com>
2026-08-13 17:42:19 +0700

    fix: restore the_ranger_react_native project directory to root level

--- the_ranger_mobile/the_ranger_react_native/the_ranger_mobile/android/app/src/main/kotlin/com/example/the_ranger_mobile/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.the_ranger_mobile
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- the_ranger_mobile/the_ranger_react_native/tmp_import_test.dart
+++
@@ -1,1 +0,0 @@
-import 'package:the_ranger_mobile/views/auth/pemilik_marketplace/mitra_marketplace_dashboard_view.dart'; void main() {}
